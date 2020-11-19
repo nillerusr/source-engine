5f12b73fd3ce1a9436f76c6ce9b5837e43bd1dcf
JusicP <slender87844@gmail.com>
2020-11-19 22:42:48 +0200

    delete shit

--- external/vpc/utils/vpc/obj/Linux/release/tier0/cpu_posix.d
+++
@@ -1,4 +0,0 @@
-obj/Linux/release/tier0/cpu_posix.d obj/Linux/release/tier0/cpu_posix.o: ../../tier0/cpu_posix.cpp \
- ../../public/tier0/platform.h ../../public/tier0/wchartypes.h \
- ../../public/tier0/valve_off.h ../../public/tier0/valve_on.h \
- ../../public/tier0/annotations.h
