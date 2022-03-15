22ed5194a697fff5b5936d5836cfac93ab93c043
nillerusr <nillerusr@users.noreply.github.com>
2022-03-15 13:09:34 +0300

    tier1: exclude steam deck from KeyValues

--- tier1/KeyValues.cpp
+++ tier1/KeyValues.cpp
@@ -2195,24 +2195,27 @@ bool EvaluateConditional( const char *str )
 	if ( *str == '!' )
 		bNot = true;
 
+	if( Q_stristr( str, "$DECK" ) )
+		return false ^ bNot; // Steam deck unsupported
+
 	if ( Q_stristr( str, "$X360" ) )
 		return IsX360() ^ bNot;
-	
+
 	if ( Q_stristr( str, "$WIN32" ) )
 		return IsPC() ^ bNot; // hack hack - for now WIN32 really means IsPC
 
 	if ( Q_stristr( str, "$WINDOWS" ) )
 		return IsWindows() ^ bNot;
-	
+
 	if ( Q_stristr( str, "$OSX" ) )
 		return IsOSX() ^ bNot;
-	
+
 	if ( Q_stristr( str, "$LINUX" ) )
 		return IsLinux() ^ bNot;
 
 	if ( Q_stristr( str, "$POSIX" ) )
 		return IsPosix() ^ bNot;
-	
+
 	return false;
 }
 
