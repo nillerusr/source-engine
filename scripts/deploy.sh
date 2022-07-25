31349038f353ced08c9064cc6b7d8eefc33dc440
nillerusr <nillerusr@gmail.com>
2022-07-25 21:26:11 +0300

    Update deploy,sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,7 @@
 
 sudo dpkg --add-architecture i386
 sudo apt-get update
+sudo apt-get build-dep -y libsdl2-dev
 sudo apt-get install -f -y g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386
 
 ./waf configure -T debug
