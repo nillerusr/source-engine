26030c37c94c16be58f31a495cb274289abfca77
nillerusr <nillerusr@gmail.com>
2022-07-25 21:21:08 +0300

    Update deploy,sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,6 @@
 
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt install -y g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386
-sudo apt install -f
+sudo apt-get install -f -y g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386
 
 ./waf configure -T debug
-
