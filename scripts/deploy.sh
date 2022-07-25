50fb8f8a65b954bb0b78d637f4c3437399d2310d
nillerusr <nillerusr@gmail.com>
2022-07-25 21:40:49 +0300

    Update deploy,sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install -f -y g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386
 
-./waf configure -T debug
+PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ./waf configure -T debug
