7afc875872e07e8064075c58686363f358bb547a
nillerusr <nillerusr@gmail.com>
2022-07-25 23:06:53 +0300

    update workflow

---
+++ scripts/build-android-armv7a.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+git submodule init && git submodule update
+sudo dpkg --add-architecture i386
+sudo apt-get update
+
+wget https://dl.google.com/android/repository/android-ndk-r10e-linux-x86_64.zip
+unzip android-ndk-r10e-linux-x86_64.zip
+export ANDROID_NDK=$PWD/android-ndk-r10e/
+./waf configure -T debug --android=armeabi-v7a-hard,4.9,21 --disable-warns &&
+./waf build
