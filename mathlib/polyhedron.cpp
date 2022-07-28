e2f577b5e2dcaa04f7fe739f842974f643fe9897
HappyDOGE <28511119+HappyDOGE@users.noreply.github.com>
2022-07-28 17:48:29 +0300

    fix placement new

--- mathlib/polyhedron.cpp
+++ mathlib/polyhedron.cpp
@@ -11,7 +11,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "tier1/utlvector.h"
-
+#ifdef COMPILER_MSVC
+#include <new>
+#endif
 
 
 struct GeneratePolyhedronFromPlanes_Point;
@@ -82,7 +84,7 @@ CPolyhedron_AllocByNew *CPolyhedron_AllocByNew::Allocate( unsigned short iVertic
 										(iIndices * sizeof( Polyhedron_IndexedLineReference_t )) + 
 										(iPolygons * sizeof( Polyhedron_IndexedPolygon_t )));
 
-	CPolyhedron_AllocByNew *pAllocated = (CPolyhedron_AllocByNew*)pMemory;
+	CPolyhedron_AllocByNew *pAllocated = new ( pMemory ) CPolyhedron_AllocByNew;
 
 	pAllocated->iVertexCount = iVertices;
 	pAllocated->iLineCount = iLines;
