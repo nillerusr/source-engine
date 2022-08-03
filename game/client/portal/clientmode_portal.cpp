48aa042ee108294aa411b15312fd26d0641fc020
SanyaSho <68691958+SanyaSho@users.noreply.github.com>
2022-08-03 16:22:17 +0300

    Fov cvar fix (#98)
    * game: set fov_desired cvar max value to 110
    * scripts: waifulib: change '4.9,9' to '4.9,21' in configure example

--- game/client/portal/clientmode_portal.cpp
+++ game/client/portal/clientmode_portal.cpp
@@ -20,8 +20,7 @@
 
 // default FOV for HL2
 ConVar default_fov( "default_fov", "75", FCVAR_CHEAT );
-ConVar fov_desired( "fov_desired", "75", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, 90.0 );
-
+ConVar fov_desired( "fov_desired", "75", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, 110.0 );
 
 // The current client mode. Always ClientModeNormal in HL.
 IClientMode *g_pClientMode = NULL;
