e8ffb402de8df6faa02fb8c3f0e10ce35a85f740
SanyaSho <gamep8118@gmail.com>
2022-08-04 16:30:30 +0300

    game: quantum crouch fix

--- game/shared/gamemovement.cpp
+++ game/shared/gamemovement.cpp
@@ -4191,8 +4191,7 @@ void CGameMovement::FinishUnDuckJump( trace_t &trace )
 //-----------------------------------------------------------------------------
 void CGameMovement::FinishDuck( void )
 {
-	if ( player->GetFlags() & FL_DUCKING )
-		return;
+	// if ( player->GetFlags() & FL_DUCKING ) return;
 
 	player->AddFlag( FL_DUCKING );
 	player->m_Local.m_bDucked = true;
