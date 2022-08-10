241f8fb00723dc2c6a94031114478451aba4b72a
SanyaSho <gamep8118@gmail.com>
2022-08-10 23:46:35 +0300

    game: fov_desired max value 110

--- game/client/tf2base/clientmode_tf.cpp
+++ game/client/tf2base/clientmode_tf.cpp
@@ -50,7 +50,7 @@
 #endif
 
 ConVar default_fov( "default_fov", "75", FCVAR_CHEAT );
-ConVar fov_desired( "fov_desired", "75", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, 90.0 );
+ConVar fov_desired( "fov_desired", "75", FCVAR_ARCHIVE | FCVAR_USERINFO, "Sets the base field-of-view.", true, 75.0, true, 110.0 );
 
 void HUDMinModeChangedCallBack( IConVar *var, const char *pOldString, float flOldValue )
 {
