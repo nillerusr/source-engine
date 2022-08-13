016b4d2189e61a08c2f6c1831988fe3aae8be486
SanyaSho <gamep8118@gmail.com>
2022-08-13 20:38:34 +0300

    game: windows-1252 again

--- game/client/tf2base/vgui/tf_vgui_video.h
+++ game/client/tf2base/vgui/tf_vgui_video.h
@@ -1,4 +1,4 @@
-//====== Copyright © 1996-2007, Valve Corporation, All rights reserved. =======
+//====== Copyright Â© 1996-2007, Valve Corporation, All rights reserved. =======
 //
 // Purpose: VGUI panel which can play back video, in-engine
 //
