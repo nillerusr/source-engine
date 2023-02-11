c48da00b92c967c61b31b17e89cb35998d4b957f
nillerusr <nillerusr@gmail.com>
2023-02-11 20:32:11 +0300

    tier0: disable vprof by default

--- public/tier0/vprof.h
+++ public/tier0/vprof.h
@@ -15,10 +15,6 @@
 #include "tier0/vprof_telemetry.h"
 
 // VProf is enabled by default in all configurations -except- X360 Retail.
-#if !( defined( _X360 ) && defined( _CERT ) )
-#define VPROF_ENABLED
-#endif
-// TODO(nillerusr): make stubbed vprofile
 
 #if defined(_X360) && defined(VPROF_ENABLED)
 #include "tier0/pmc360.h"
