b2d168c70120143246aa501146e94d696e4e6bed
nillerusr <nillerusr@gmail.com>
2023-04-07 15:03:12 +0300

    scripts: remove useless deps from installation

--- scripts/tests-ubuntu-i386.sh
+++ scripts/tests-ubuntu-i386.sh
@@ -3,8 +3,7 @@ install#!/bin/sh
 git submodule init && git submodule update
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get install -y aptitude
-sudo aptitude install -y libopenal-dev:i386 g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386 libedit-dev:i386
+sudo apt-get install -y g++-multilib gcc-multilib libbz2-dev:i386
 
 PKG_CONFIG_PATH=/usr/lib/i386-linux-gnu/pkgconfig ./waf configure -T release --sanitize=address,undefined --disable-warns --tests --prefix=out/ $* &&
 ./waf install &&
