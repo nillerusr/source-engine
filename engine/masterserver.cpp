8f8c2556b75cf6d5bc139a94b65a28cba51aba59
AruMoon <anton17082003@gmail.com>
2023-10-25 05:18:51 +0500

    Add new master server

--- engine/masterserver.cpp
+++ engine/masterserver.cpp
@@ -31,7 +31,8 @@ extern ConVar sv_lan;
 static char g_MasterServers[][64] =
 {
 	"185.192.97.130:27010",
-	"168.138.92.21:27016"
+	"168.138.92.21:27016",
+	"135.125.188.162:27010"
 };
 
 #ifdef DEDICATED
