06f9229623ceaf954fb35dcadbd7752ccc8be5ed
user <user@email.com>
2024-12-26 16:34:19 +0300

    Add support for all default systems

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
 
     ivp-submodule = {
       url = "github:nillerusr/source-physics";
@@ -18,45 +19,47 @@
     };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-  let
-    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+  flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
 
-    # Fix for git submodules
-    hl2-unwrapped = (pkgs.callPackage ./hl2-unwrapped.nix { }).overrideAttrs {
-      postPatch = ''
-        rm -rf ./{ivp,lib,thirdparty}
-        ln -s ${inputs.ivp-submodule} ./ivp
-        ln -s ${inputs.lib-submodule} ./lib
-        ln -s ${inputs.thirdparty-submodule} ./thirdparty
-      '';
-    };
-  in
-  {
-    # Install the game on your own computer
-    packages.x86_64-linux = {
-      hl2 = pkgs.callPackage ./build.nix { inherit hl2-unwrapped; };
-      default = self.packages.x86_64-linux.hl2;
-    };
+      # Fix for git submodules
+      hl2-unwrapped = (pkgs.callPackage ./hl2-unwrapped.nix { }).overrideAttrs {
+        postPatch = ''
+          rm -rf ./{ivp,lib,thirdparty}
+          ln -s ${inputs.ivp-submodule} ./ivp
+          ln -s ${inputs.lib-submodule} ./lib
+          ln -s ${inputs.thirdparty-submodule} ./thirdparty
+        '';
+      };
+    in
+    {
+      # Install the game on your own computer
+      packages = {
+        hl2 = pkgs.callPackage ./build.nix { inherit hl2-unwrapped; };
+        default = self.packages.${system}.hl2;
+      };
 
-    # Build the game for others
-    devShells.default = pkgs.mkShell rec {
-      nativeBuildInputs = with pkgs; [
-        makeWrapper
-        SDL2
-        freetype
-        fontconfig
-        zlib
-        bzip2
-        libjpeg
-        libpng
-        curl
-        openal
-        libopus
-        pkg-config
-        gcc
-        python3
-      ];
-    };
-  };
+      # Build the game for others
+      devShells.default = pkgs.mkShell rec {
+        nativeBuildInputs = with pkgs; [
+          makeWrapper
+          SDL2
+          freetype
+          fontconfig
+          zlib
+          bzip2
+          libjpeg
+          libpng
+          curl
+          openal
+          libopus
+          pkg-config
+          gcc
+          python3
+        ];
+      };
+    }
+  );
 }
