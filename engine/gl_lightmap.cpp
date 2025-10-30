fbcab128496f37f81fd692f5fbb9dd52057c9cb7
Ryssiaczrk <fajnyziom1000@gmail.com>
2025-10-30 16:03:32 -0700

    Fix: lightmap

--- engine/gl_lightmap.cpp
+++ engine/gl_lightmap.cpp
@@ -1350,7 +1350,7 @@ static void SortSurfacesByLightmapID( SurfaceHandle_t *pToSort, int iSurfaceCoun
 
 		int iCounts[256] = { 0 };
 		int iBitOffset = radix * 8;
-		for( int i = 0; i != iSurfaceCount; ++i )
+		for( int i = 0; i < iSurfaceCount; ++i )
 		{
 			uint8 val = (materialSortInfoArray[MSurf_MaterialSortID( pSortTemp[i] )].lightmapPageID >> iBitOffset) & 0xFF;
 			++iCounts[val];
@@ -1358,12 +1358,12 @@ static void SortSurfacesByLightmapID( SurfaceHandle_t *pToSort, int iSurfaceCoun
 
 		int iOffsetTable[256];
 		iOffsetTable[0] = 0;
-		for( int i = 0; i != 255; ++i )
+		for( int i = 0; i < 255; ++i )
 		{
 			iOffsetTable[i + 1] = iOffsetTable[i] + iCounts[i];
 		}
 
-		for( int i = 0; i != iSurfaceCount; ++i )
+		for( int i = 0; i < iSurfaceCount; ++i )
 		{
 			uint8 val = (materialSortInfoArray[MSurf_MaterialSortID( pSortTemp[i] )].lightmapPageID >> iBitOffset) & 0xFF;
 			int iWriteIndex = iOffsetTable[val];
