32a81415040376d3c5e620f4a7418eda248a41c8
celisej567 <celisej567@gmail.com>
2025-11-28 02:03:18 +0300

    Fix text selection at RichText.

--- vgui2/vgui_controls/RichText.cpp
+++ vgui2/vgui_controls/RichText.cpp
@@ -862,6 +862,12 @@ void RichText::Paint()
 		if ( m_LineBreaks.IsValidIndex( lineBreakIndexIndex ) && m_LineBreaks[lineBreakIndexIndex] < iLim )
 			iLim = m_LineBreaks[lineBreakIndexIndex];
 
+		// Stop when entering or exiting the selected range
+		if ( i < selection0 && iLim >= selection0 )
+			iLim = selection0;
+		if ( i >= selection0 && i < selection1 && iLim >= selection1 )
+			iLim = selection1;
+
 		// Handle non-drawing characters specially
 		for ( int iT = i; iT < iLim; iT++ )
 		{
